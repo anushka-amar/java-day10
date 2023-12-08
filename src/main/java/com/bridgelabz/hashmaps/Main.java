e1d97e5c5ce12c5fc063f3293c1c53cbf52705fd
Anushka <anushkamar1102@gmail.com>
2023-12-08 11:32:19 +0530

    Refactored: UC2 Ability to find frequency in a large paragraph phrase

--- src/main/java/com/bridgelabz/hashmaps/Main.java
+++ src/main/java/com/bridgelabz/hashmaps/Main.java
@@ -12,7 +12,20 @@ public class Main {
             hashmap.addWord(word.toLowerCase());
         }
 
-        System.out.println("The frequency of words: ");
+        System.out.println("The frequency of words in the string: ");
         hashmap.displayFreq();
+
+        String paragraph = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
+
+        MyHashMap hashMap2 = new MyHashMap(20);
+        String[] newWords = paragraph.split("\\s+");
+
+        for (String word : newWords){
+            hashMap2.addWord(word.toLowerCase());
+        }
+
+        System.out.println("\n\nThe frequency of words in the paragraph is:-");
+        hashMap2.displayFreq();
     }
 }
